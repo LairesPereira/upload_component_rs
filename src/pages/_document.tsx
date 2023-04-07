564ef37f9d27650ee4b9d3ebb3c2bc1a2cc50e51
Laires <Dev@MacBook-Air-de-Laires.local>
2023-04-06 21:55:11 -0300

    draggable area interface implemented

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='h-full bg-slate-400'>
+      <body className='h-full bg-[#FBFAFF]'>
         <NextScript />
       </body>
     </Html>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image"
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+
 export default function Home() {
   return (
-    <main className="h-full bg-slate-400">
-     <div className="bg-sky-500/50 h-full">
-      <div className='flex place-content-center grid-cols-1 draggableArea'>
-        <span>Hello!</span>
+    <main className={`${inter.className} grid h-screen place-items-center bg-gradient-to-r from-[#A8CAFF] to-[#DAE8FF]`}>
+     <div className="border-2 border-dashed rounded-lg bg-[#FBFAFF]">
+      <div className='flex flex-col place-content-center grid-cols-1 draggableArea'>
+        <div className="icon flex place-content-center px-20 pt-8 pb-3">
+        <Image src="/upload.png" alt="upload file" width={60} height={60} />
+        </div>
+        <span className="px-20 pb-14">Import or drag your files here</span>
       </div>
      </div>
     </main>
